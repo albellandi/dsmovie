a76e8f92341087b654032b0f1c4ed7ad683b47a3
Alexandre Bellandi <bellandi@gmail.com>
2022-03-08 10:24:25 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import {ReactComponent as GithubICon} from 'assets/img/github.svg';
+import './styles.css'
+
+function Navbar(){
+  return (  
+    <header>
+        <nav className='container'>
+        <div className='dsmovie-nav-content'>
+            <h1>DSMovie</h1>
+            <a href="https://github.com/albellandi">
+                <div className='dsmovie-contact-container'>
+                    <GithubICon />
+                    <p className='dsmovie-contact-link'>/devsuperior</p>
+                </div>
+                </a>
+        </div>
+        </nav>
+    </header>
+  );  
+}
+
+export default Navbar;
