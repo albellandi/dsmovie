90af141362888d4b688df5fa9df800d47ce16721
Alexandre Bellandi <bellandi@gmail.com>
2022-03-09 14:44:30 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entititeis/Score.java
@@ -0,0 +1,42 @@
+package com.devsuperior.dsmovie.entititeis;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+	
+	@EmbeddedId
+	private ScorePk id = new ScorePk();
+	private Double value;
+	
+	
+	public Score() {
+	}
+	
+	public ScorePk getId() {
+		return id;
+	}
+	public void setId(ScorePk id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+	
+
+}
